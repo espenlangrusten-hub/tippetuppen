892d35c339516f44e1cdcee9c3b1d406639c9dcc
espenlangrusten-hub <espen.langrusten@gmail.com>
2026-09-16 16:11:44 +0200

    Kjappen: last inn dataspill-stilen

--- src/app/kjappen/page.tsx
+++ src/app/kjappen/page.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./kjappen-v2.css";
 import "./kjappen-type.css";
 import "./kjappen-arcade.css";
+import "./kjappen-game.css";
 import { KjappenGame } from "./KjappenGame";
 
 // Kept out of the index while Kjappen is being polished and tested.
