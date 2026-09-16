44de0a1c6147a700dbe28eefcc176f6dd32a4919
espenlangrusten-hub <espen.langrusten@gmail.com>
2026-09-16 17:32:00 +0200

    Kjappen: last inn frameløs programleder sist

--- src/app/kjappen/page.tsx
+++ src/app/kjappen/page.tsx
@@ -5,6 +5,7 @@ import "./kjappen-arcade.css";
 import "./kjappen-game.css";
 import "./kjappen-lobby.css";
 import "./kjappen-responsive.css";
+import "./kjappen-host.css";
 import { KjappenGame } from "./KjappenGame";
 
 // Kept out of the index while Kjappen is being polished and tested. Viewport-fit pass active.
